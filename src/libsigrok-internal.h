03da25aeb24c40265a96bbccced035fe5b5b05ee
unknown <Maintenance@ACCOM-WOLF-005.campus.gla.ac.uk>
2023-02-05 22:35:41 +0000

    fix device.c properly

--- src/libsigrok-internal.h
+++ src/libsigrok-internal.h
@@ -1637,7 +1637,7 @@ enum {
 };
 
 SR_PRIV struct sr_channel *sr_channel_new(struct sr_dev_inst *sdi,
-		int index, int type, gboolean enabled, const char *name, gboolean fx3);
+		int index, int type, gboolean enabled, const char *name);
 SR_PRIV void sr_channel_free(struct sr_channel *ch);
 SR_PRIV void sr_channel_free_cb(void *p);
 SR_PRIV struct sr_channel *sr_next_enabled_channel(const struct sr_dev_inst *sdi,
